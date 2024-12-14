a1ba00055aec1c20fd46a6a087c45439e92d94dd
Kirill <okaydiscord@mail.ru>
2024-12-14 18:12:43 +0300

    Changing the function to encode a voice message from the user.

--- src/public/styles/chat.js
+++ src/public/styles/chat.js
@@ -34,8 +34,6 @@ document.addEventListener("DOMContentLoaded", function() {
     let audioChunks = [];
     let isRecording = false;
 
-    // Передаем user.id из HTML в JavaScript
-    const userId = "{{ user.id }}";
 
     themeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-theme');
